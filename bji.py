22cb0ec6d304169949d0c6d8fa8d892ba294f6b2
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-07-26 16:20:16 +0200

    Fixes #2

--- bji.py
+++ bji.py
@@ -21,15 +21,29 @@ class JobRegistry(object):
     jobs = {}
     parsed_once = False
 
+    class CancelledJob(object):
+        """Dummy class indicating a manually closed job indicator.
+        Allows to call set_state which is a no-op."""
+        def set_state(self, *args):
+            pass
+
     def close_callback(self, jobid):
-        del self.jobs[jobid]
+        # Indicator closed: Replace JIndicator
+        # with dummy class allowing to call set_state() on it
+        # and indicating that the job indicator has been closed manually.
+        self.jobs[jobid] = JobRegistry.CancelledJob()
+
+    def _all_jobs_cancelled(self):
+        return all(map(lambda v: type(v) is JobRegistry.CancelledJob, self.jobs.values()))
 
     def empty(self):
-        return self.parsed_once and not bool(self.jobs)
+        # Holds only closed jobs and at least one job has been registered in the past
+        return self.parsed_once and self._all_jobs_cancelled()
 
     def handle_job(self, jobid, state):
         self.parsed_once = True
         if jobid in self.jobs:
+            # May also be called on cancelled jobs
             self.jobs[jobid].set_state(state)
         else:
             self.jobs[jobid] = JIndicator(jobid, state, self.close_callback)
